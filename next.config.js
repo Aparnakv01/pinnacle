c2d65ed83018b9e887f304ac381e4dbed6e8afc2
Aparnakv01 <aparna.kv@zylensolutions.com>
2026-05-11 11:53:07 +0530

    domain change

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
       beforeFiles: [
         {
           source: '/insights/:slug*',
-          destination: 'https://dev-api.topgeo.ai/cms/render/my-web-app--project-1087982776454717284.asia-southeast1.hosted.app/:slug*',
+          destination: 'https://dev-api.topgeo.ai/cms/render/pinnacle-one-lac.vercel.app/:slug*',
         },
       ],
     };
